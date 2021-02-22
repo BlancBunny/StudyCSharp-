6a74df7c184d681dfe36c7f775bc0f6b37f03f99
BlancBunny <nhsk0767@gmail.com>
2021-02-22 17:06:00 +0900

    2021-02-22 수업 끝

---
+++ chapter03/Chapter03App/ConstTestApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConstTestApp
+{
+    class Program
+    {
+
+        enum Season
+        {
+            Spring = 1000,
+            Summer = 2000,
+            Fall = 3000,
+            Winter = 4000
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("상수테스트");
+            const double pi = 3.141592;  //  상수 (불변)
+            Console.WriteLine($"원주율의 값은 {pi}");
+            Season mySeason = Season.Summer;
+            Console.WriteLine($"지금 계절은 {mySeason} 입니다");
+            
+            int? a = 2;
+        
+            Console.WriteLine($"a = {a.HasValue}"); 
+
+        }
+    }
+}
---
+++ chapter03/Chapter03App/IntConversionApp/ConvTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntConversionApp
+{
+    class ConvTest
+    {
+        static void Main(string[] args)
+        {
+            short shMaxVal = short.MaxValue;
+            int inCastVal = 0;
+
+            inCastVal = shMaxVal;
+            Console.WriteLine($"short값 변환한 int값 : {inCastVal}");
+
+            inCastVal += 5;
+            short shCastVal = (short)inCastVal;
+            Console.WriteLine($"int 값 변환한 short 값 : {shCastVal}");
+
+            // float, double (실수형) 
+            float flVal = 3.141592f;
+            inCastVal = (int)flVal;
+            Console.WriteLine($"float값 변환한 int 값 : {inCastVal}");
+            
+            // obj : 
+            object obj = 20; // boxing 
+            int intUnboxingVal = (int)obj;
+
+            // str -> int 
+            string strVal = "200";
+            int result = int.Parse(strVal) * 3;
+            Console.WriteLine($"result = {result}");
+        }
+    }
+}
