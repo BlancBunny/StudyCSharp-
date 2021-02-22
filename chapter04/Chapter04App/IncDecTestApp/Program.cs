1ac87cda613ee399d907aad5baf88bca3824d85a
BlancBunny <nhsk0767@gmail.com>
2021-02-22 17:27:30 +0900

    2021-02-22 수업 끝

---
+++ chapter04/Chapter04App/IncDecTestApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace IncDecTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("증감연산자 테스트");
+
+            int result = 26;
+            Console.WriteLine($"result = {result}");
+            // +=, -=, *=, /= 
+            // ++, -- 
+            // 
+        }
+    }
+} 
