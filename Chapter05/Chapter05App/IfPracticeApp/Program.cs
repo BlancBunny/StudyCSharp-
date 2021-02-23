e3a7e175121833402f7e8eb3238fe2ace80c5f9a
BlancBunny <nhsk0767@gmail.com>
2021-02-23 12:38:19 +0900

    2021-02-23 오전 수업 끝

---
+++ Chapter05/Chapter05App/IfPracticeApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfPracticeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("수를 입력하세요 : ");
+                string line = Console.ReadLine();
+
+                if (line == "quit") break;
+                
+                // int number = int.Parse(line);
+                int number = 0;
+                int.TryParse(line, out number);
+                // Console.WriteLine(number);
+                if (number >= 0)
+                {
+                    if (number % 2 == 0) Console.WriteLine("양의 짝수");
+                    else Console.WriteLine("양의 홀수");
+                }
+                else Console.WriteLine("0보다 작은 수");
+            }
+            Console.WriteLine("계산 종료!");
+        }
+    }
+}
---
+++ Chapter05/Chapter05App/LoopTestApp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           /* // while
+            int a = 10;
+            while (a>0)
+            {
+                Console.WriteLine($"숫자는 {a--}");
+                a -= 2;
+            }
+            Console.WriteLine("출력 종료!");
+
+            do // do-while : 최초 1회 실행 후 조건문 검사 
+            {
+                Console.WriteLine($"숫자 {a}");
+                a++;
+            } while (a < 10);
+           
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"i 값은 {i}");
+            }*/
+
+            for (int i = 1; i < 10; i++)
+            {
+                for (int j = 1; j < 10; j++)
+                {
+                    Console.Write($"{i} x {j} = {i*j} ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+
+    }
+}
