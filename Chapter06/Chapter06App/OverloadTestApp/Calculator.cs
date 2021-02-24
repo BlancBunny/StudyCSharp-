ecf19b298787ec5533a623d18894494a45f3335b
BlancBunny <nhsk0767@gmail.com>
2021-02-24 10:41:52 +0900

    2021-02-24 6장 끝

---
+++ Chapter06/Chapter06App/OverloadTestApp/Calculator.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadTestApp // 메소드 오버로딩 : 같은 이름의 메소드를 
+                          // 다른 자료형의 같은 동작으로 만들 수 있다. 
+{
+    class Calculator
+    {
+        static void Main(string[] args)
+        {
+            int x = Calculator.Plus(3, 4);
+            Console.WriteLine($"3 + 4 = {x}");
+            float y = Calculator.Plus(3.4f, 4.12f);
+            Console.WriteLine($"3.4 + 4.12 = {y}");
+            double z = Calculator.Plus(6.122, 2.24);
+            Console.WriteLine($"6.122 + 2.24 = {z}");
+            Console.WriteLine($"{Calculator.Plus(3.14, "5")}");
+
+            int w = Calculator.Plus(3, 4, 5);
+            Console.WriteLine($"3+4+5={w}");
+
+            // 가변길이 매개변수 
+            Console.WriteLine(Sum(1,3,4,5,6,7));
+
+        }
+
+        private static int Sum(params int[] args)
+        {
+            int result = 0;
+            foreach (var item in args)
+            {
+                result += item;
+            }
+            return result;
+        }
+
+        private static int Plus(int v1, int v2, int v3)
+        {
+            return v1 + v2 + v3;
+        }
+
+        private static object Plus(double v1, string v2)
+        {
+            double.TryParse(v2, out double p2);
+            return v1 + p2;
+        }
+        private static double Plus(double v1, double v2)
+        {
+            return v1 + v2;
+            // throw new NotImplementedException();
+        }
+        private static float Plus(float v1, float v2)
+        {
+            return v1 + v2;
+            //throw new NotImplementedException();
+        }
+        private static int Plus(int v1, int v2)
+        {
+            return v1 + v2;
+            // throw new NotImplementedException();
+        }
+    }
+}
--- Chapter06/Chapter06App/ProfileTestApp/Program.cs
+++ Chapter06/Chapter06App/ProfileTestApp/Program.cs
@@ -11,13 +11,22 @@ namespace ProfileTestApp
         static void Main(string[] args)
         {
             PrintProfile("김현수", "010-4942-0767");
+            // 일반적인 호출 (매개변수 순서에 맞춰 입력)
             if (PrintProfile(null, "010-1111-2222") == -1)
             {
                 Console.WriteLine("프로필 출력 시 오류 발생!");
             }
+
+            PrintProfile(phone: "010-4942-0767", name: "김현수");
+            // 명명된 매개변수 
+
+            PrintProfile("홍길동");
+            // 매개변수 디폴트값 설정 가능 
         }
 
-        public static int PrintProfile(string name, string phone)
+       
+
+        public static int PrintProfile(string name, string phone = "010-7979-2424")
         {
             if (string.IsNullOrEmpty(name)) {
                 Console.WriteLine("이름을 정확히 입력하세요.");
