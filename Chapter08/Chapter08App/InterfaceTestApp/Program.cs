0ad1adcc43be88b47277d64accb33713bb67099a
BlancBunny <nhsk0767@gmail.com>
2021-02-25 15:22:17 +0900

    2021-02-25 8장 완료

--- Chapter08/Chapter08App/InterfaceTestApp/Program.cs
+++ Chapter08/Chapter08App/InterfaceTestApp/Program.cs
@@ -6,10 +6,57 @@ using System.Threading.Tasks;
 
 namespace InterfaceTestApp
 {
+    interface ILogger
+    {
+        void WriteLog(string msg);
+    }
+
+    class ConsoleLogger : ILogger
+    {
+        public void WriteLog(string msg)
+        {
+            Console.WriteLine($"[{DateTime.Now.ToShortDateString()}] log - {msg}");
+        }
+    }
+
+    class CustomLogger : ILogger
+    {
+        public void WriteLog(string msg)
+        {
+            Console.WriteLine($"log - {msg}\a\a");
+        }
+    }
+    class ClimateLogger
+    {
+        private ILogger logger;
+        public ClimateLogger(ILogger logger)
+        {
+            this.logger = logger; 
+        }
+        public void Start()
+        {
+            while(true)
+            {
+                Console.Write($"온도를 입력 : ");
+                string temp = Console.ReadLine();
+                if (string.IsNullOrEmpty(temp)) break;
+                logger.WriteLog($"현재온도 : {temp}도");
+            }
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            ILogger logger = new ConsoleLogger();
+            logger.WriteLog("new log 1");
+            // 내가 직접 코드로 로그를 입력 
+
+            ClimateLogger clogger = new ClimateLogger(new CustomLogger());
+            clogger.Start();
+            // 프레임워크(ClimateLogger)가 로그를 입력 
+            // IoC (Inversion of Control) : 제어 역전 
         }
     }
 }
---
+++ Chapter08/Chapter08App/MultiInterfaceApp/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace MultiInterfaceApp
+{
+    interface IRunnerable // 지상
+    {
+        void Run();
+    }
+
+    interface IFlyable // 공중 
+    {
+        void Fly();
+    }
+
+    class DroneCar : IRunnerable, IFlyable // 다중 인터페이스 
+    {
+        public void Run()
+        {
+            Console.WriteLine("Execute Run!");
+        }
+
+        public void Fly()
+        {
+            Console.WriteLine("Execute Fly!");
+        }
+
+        
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Drone Test");
+            DroneCar dc = new DroneCar();
+            dc.Run();
+            dc.Fly();
+
+            Console.WriteLine("자동차로 변경(interface)");
+            IRunnerable car = dc;  
+            // dc는 IRunnerable, IFlyable의 메소드을 모두 가지고 있지만
+            // car는 IRunnerable의 메소드만 가지므로 그 부분만 넘어간다 
+            
+            car.Run();
+            // car.Fly();    << 에러 발생 
+            
+
+            Console.WriteLine("비행기로 변경(interface)");
+            IFlyable plane = dc;
+            plane.Fly();
+        }
+    }
+}
