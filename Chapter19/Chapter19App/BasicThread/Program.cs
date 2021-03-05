69b2be79221cc816c11ba2846d753a39bebaa2fc
BlancBunny <nhsk0767@gmail.com>
2021-03-05 12:42:02 +0900

    2021-03-05 19장 스레드

---
+++ Chapter19/Chapter19App/BasicThread/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+
+namespace BasicThread
+{
+    
+    class Program
+    {
+        static void DoSomething()
+        {
+            for (int i = 0; i < 50; i++)
+            {
+                Console.WriteLine($"Do Something : {i}");
+                Thread.Sleep(10);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(new ThreadStart(DoSomething));
+            Console.WriteLine("스레드 시작");
+            t1.Start();
+
+            for (int i = 0; i < 50; i++)
+            {
+                Console.WriteLine($"Main Thread : {i}");
+                Thread.Sleep(10);
+            }
+
+            Console.WriteLine("스레드 종료 대기");
+            t1.Join();
+
+            Console.WriteLine("프로세스 종료");
+
+        }
+    }
+}
---
+++ Chapter19/Chapter19App/SideTaskApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+
+namespace SideTaskApp
+{
+    class SideTask
+    {
+        int Count { get; set; }
+        
+        public SideTask(int count)
+        {
+            this.Count = count;
+        }
+
+        public void KeepAlive()
+        {
+            while(Count>0)
+            {
+                Console.WriteLine($"{Count} 남음");
+                Count--;
+                Thread.Sleep(10);
+            }
+            Console.WriteLine($"Count : {Count}");
+        }
+
+        public void CountDown()
+        {
+            while(Count>0)
+            {
+                Console.WriteLine($"{Count} 남음");
+
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SideTask task = new SideTask(100);
+            Thread th = new Thread(task.CountDown);
+        }
+    }
+}
