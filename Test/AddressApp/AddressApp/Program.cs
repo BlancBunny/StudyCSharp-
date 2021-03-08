b0c4712bdb9546f79def5cfca3a0057f388fbd73
BlancBunny <nhsk0767@gmail.com>
2021-03-08 15:31:56 +0900

    1

--- Test/AddressApp/AddressApp/Program.cs
+++ Test/AddressApp/AddressApp/Program.cs
@@ -10,19 +10,8 @@ namespace AddressApp
     {
         static void Main(string[] args)
         {
-            Control c = new Control();
-            List<Address> addressArray = c.ListLoad();
-            // 파일로부터 리스트 불러오기
-
-<<<<<<< HEAD
-            c.Start(addressArray);
-=======
-            c.Start(ref addressArray);
->>>>>>> c64e6853cacbe6ebc803e715b934540800e7c330
-            // 루프 실행 
-
-            c.ListSave(addressArray);
-            // 리스트를 파일로 저장 
+            Menu m = new Menu();
+            m.Start();
         }
     }
 }
